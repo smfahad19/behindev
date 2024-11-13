aa2b36091e50d8691c93ed098bb4e17858955af1
smfahad19 <156584040+smfahad19@users.noreply.github.com>
2024-11-14 00:11:46 +0500

    Update Imageslider.jsx

--- src/components/Imageslider.jsx
+++ src/components/Imageslider.jsx
@@ -3,17 +3,17 @@ import secondimage from '../assets/second pic.png';
 
 const ImageSlider = () => {
   return (
-    <div className="h-screen w-full relative bg-gradient-to-b from-[#545454] to-[#28bab1]">
-      <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 z-0 overflow-hidden">
+<div  style={{ background: 'linear-gradient(black, #2ABAB1)' }} className="h-screen w-full relative">
+<div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 z-0 overflow-hidden">
         <div className="whitespace-nowrap animate-marquee text-white text-xl font-serif">
-          <p className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
+          <p className="text-6xl sm:text-7xl font italic md:text-8xl lg:text-9xl">
             Best Dev Stories Platform
           </p>
         </div>
       </div>
 
       <div className="flex justify-center items-center h-full z-10 relative">
-        <img className="w-full max-w-[1120px] h-auto" src={secondimage} alt="Second Image" />
+        <img className="w-full mt-[75px] max-w-[1000px] h-auto" src={secondimage} alt="Second Image" />
       </div>
     </div>
   );
